e4cc4a8024400b37eddcd5ae6aeaa6ecdd0e1a1b
ujangbedog <domba88galing@gmail.com>
2025-05-25 21:58:58 +0700

    chore(next): enable webpack bundle analyzer in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
+const withAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const baseConfig = {
   reactStrictMode: true,
 }
+
+const nextConfig = withAnalyzer(baseConfig)
 
 if (process.env.NODE_ENV === 'development') {
   await setupDevPlatform()
