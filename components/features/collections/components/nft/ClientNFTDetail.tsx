3243e646e9d721274a00445d7b7ebd30ffad1212
ujangbedog <domba88galing@gmail.com>
2025-05-25 15:04:24 +0700

    feat(nft): add ClientNFTDetail client component

---
+++ components/features/collections/components/nft/ClientNFTDetail.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { NFTDetail } from "@/components/features/collections";
+
+// Type definition for NFT history
+type HistoryItem = {
+  type: string;
+  from: string;
+  to: string;
+  date: string;
+  price?: string;
+};
+
+// Type definition for NFT
+type NFTItem = {
+  name: string;
+  description: string;
+  type: string;
+  creator: string;
+  owner: string;
+  mintDate: string;
+  tokenId: string;
+  blockchain: string;
+  image: string;
+  attributes: Record<string, string>;
+  history?: HistoryItem[];
+};
+
+interface ClientNFTDetailProps {
+  collectionId: string;
+  nftId: string;
+  nft: NFTItem;
+  collectionName: string;
+}
+
+export const ClientNFTDetail = ({
+  collectionId,
+  nftId,
+  nft,
+}: ClientNFTDetailProps) => {
+  return (
+    <>
+      <NFTDetail collectionId={collectionId} nftId={nftId} nft={nft} />
+    </>
+  );
+};
