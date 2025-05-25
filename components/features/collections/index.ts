ac8fd4dfa1398ae19714b6b8a5466376e9ebc531
ujangbedog <domba88galing@gmail.com>
2025-05-25 15:04:18 +0700

    refactor(collections/index): update exports and structure for new client components

--- components/features/collections/index.ts
+++ components/features/collections/index.ts
@@ -4,3 +4,10 @@ export { CollectionDetail } from "./CollectionDetail";
 export { NFTMintForm } from "./NFTMintForm";
 export { NFTDetail } from "./NFTDetail";
 export * from "./types";
+
+// Client components for interactive parts
+export { CollectionErrorMessage } from "./components/errors/CollectionErrorMessage";
+export { NFTErrorMessage } from "./components/errors/NFTErrorMessage";
+export { ClientNFTDetail } from "./components/nft/ClientNFTDetail";
+export { ClientCollectionDetail } from "./components/collection/ClientCollectionDetail";
+export { ClientNFTMintForm } from "./components/nft/ClientNFTMintForm";
