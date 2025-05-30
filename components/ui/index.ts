cc4ad08308464859e58d79f858b7a238c241aeb6
ujangbedog <domba88galing@gmail.com>
2025-05-30 17:52:23 +0700

    refactor(ui): export NextImage from UI index

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,14 +2,15 @@
  * Re-export UI components for easier imports
  */
 
-export * from "./button";
-export * from "./card";
-export * from "./container";
-export * from "./dialog";
-export * from "./dropdown-menu";
-export * from "./input";
-export * from "./toggle";
-export * from "./MetaMaskIcon";
+export * from './button';
+export * from './card';
+export * from './container';
+export * from './dialog';
+export * from './dropdown-menu';
+export * from './input';
+export * from './toggle';
+export * from './MetaMaskIcon';
+export * from './NextImage';
 
 // Re-export Windows 98 specific components
-export * from "./win98";
+export * from './win98';
