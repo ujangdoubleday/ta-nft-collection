139a32c499605c460d29108dd79e4aa28eaa9130
ujangbedog <domba88galing@gmail.com>
2025-06-07 22:36:47 +0700

    chore(collections): empty component to use action in serialize

--- components/features/collections/collection/empty/EmptyCollectionContent.tsx
+++ components/features/collections/collection/empty/EmptyCollectionContent.tsx
@@ -1,23 +1,30 @@
 'use client';
 
 import { Button } from '@/components/ui/atoms/button';
+import { Win98Window } from '@/components/ui/organisms/Win98Window';
 
 interface EmptyCollectionContentProps {
-  onAddNew: () => void;
+  onAddNewAction: () => void;
 }
 
-export function EmptyCollectionContent({ onAddNew }: EmptyCollectionContentProps) {
+export function EmptyCollectionContent({ onAddNewAction }: EmptyCollectionContentProps) {
   return (
-    <div className="border-[2px] border-t-[#808080] border-l-[#808080] border-r-white border-b-white p-6 bg-white text-center">
-      <p className="text-black text-sm mb-3">
-        This collection is empty. Start creating your digital artwork!
-      </p>
-      <Button
-        className="hover:bg-[#d0d0d0] hover:shadow-[1px_1px_0px_rgba(0,0,0,0.1)_inset]"
-        onClick={onAddNew}
-      >
-        Create Your First Artwork
-      </Button>
-    </div>
+    <Win98Window
+      title="Collections"
+      icon="/assets/icons/window/gallery.png"
+      className="max-w-12xl mx-auto"
+    >
+      <div className="p-4 text-center">
+        <p className="text-black text-sm mb-4">
+          Your gallery is empty. Start your creative journey today!
+        </p>
+        <Button
+          className="hover:bg-[#d0d0d0] hover:shadow-[1px_1px_0px_rgba(0,0,0,0.1)_inset]"
+          onClick={onAddNewAction}
+        >
+          Create Your First Artwork
+        </Button>
+      </div>
+    </Win98Window>
   );
 }
--- components/features/collections/collection/empty/EmptyCollectionMessage.tsx
+++ components/features/collections/collection/empty/EmptyCollectionMessage.tsx
@@ -2,16 +2,23 @@
 
 import Link from 'next/link';
 import { Button } from '@/components/ui/atoms/button';
+import { Win98Window } from '@/components/ui/organisms/Win98Window';
 
 export function EmptyCollectionMessage() {
   return (
-    <div className="p-4 text-center">
-      <p className="text-black text-sm mb-4">
-        Your gallery is empty. Start your creative journey today!
-      </p>
-      <Link href="/collections/new">
-        <Button>Create Your First Collection</Button>
-      </Link>
-    </div>
+    <Win98Window
+      title="Collection: Empty"
+      icon="/assets/icons/window/gallery.png"
+      className="max-w-12xl mx-auto"
+    >
+      <div className="p-4 text-center">
+        <p className="text-black text-sm mb-4">
+          Your gallery is empty. Start your creative journey today!
+        </p>
+        <Link href="/collections/new">
+          <Button>Create Your First Collection</Button>
+        </Link>
+      </div>
+    </Win98Window>
   );
 }
