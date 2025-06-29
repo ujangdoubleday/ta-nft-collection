c3cd9fcc32c2f50b68edbec50675b3aa3d8ef059
ujangbedog <domba88galing@gmail.com>
2025-06-29 18:28:42 +0700

    chore: update nft factory to use new functions

--- lib/blockchain/hooks/useNFTFactory.ts
+++ lib/blockchain/hooks/useNFTFactory.ts
@@ -1,12 +1,4 @@
-import { useCallback, useState } from 'react';
-import { useWriteContract, useWaitForTransactionReceipt } from 'wagmi';
-import { sepolia } from 'wagmi/chains';
 import { useCreateCollection } from './useNFTFactoryWrite';
-
-// Import ABIs from the Hardhat-compiled contracts
-// @ts-ignore - This will be imported properly as JSON
-import NFT_FACTORY_ABI from '../abi/NFTFactory.json';
-import { NFT_FACTORY_ADDRESS } from '@/lib/blockchain';
 
 export interface UseNFTFactoryReturn {
   createCollection: (
