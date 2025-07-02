f9503285bdf8e40a714d9da7796041089359acd8
ujangbedog <domba88galing@gmail.com>
2025-07-02 14:57:34 +0700

    new page at public route and layout

---
+++ app/(public)/page.tsx
@@ -0,0 +1,9 @@
+import { HeroSection } from '@/components/features/home/HeroSection';
+
+export default function Home() {
+  return (
+    <main className="h-screen flex flex-col overflow-hidden m-0 p-0">
+      <HeroSection />
+    </main>
+  );
+}
