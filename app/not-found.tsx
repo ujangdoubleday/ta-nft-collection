fe4c7a0845e38b182799266f880478b6413111ad
ujangbedog <domba88galing@gmail.com>
2025-07-06 02:03:11 +0700

    feat: conventional not found

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black text-white text-center px-4">
+      <h1 className="text-7xl font-bold mb-4">404</h1>
+      <p className="text-lg mb-2 italic">what are you lookin&apos; for?</p>
+
+      <p className="mt-6 text-sm text-zinc-400">
+        you'll be safe in{' '}
+        <Link
+          href="/"
+          className="underline underline-offset-4 hover:text-white transition font-medium"
+        >
+          Home.
+        </Link>
+      </p>
+    </div>
+  );
+}
