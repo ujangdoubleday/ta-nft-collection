8a8bd18743334c50ffefc83c652dcc2f2b5b781d
ilham alfath <97936093+ujangbedog@users.noreply.github.com>
2025-07-05 22:50:27 +0700

    feat(user): add TRPC router and username setup (#51)
    * feat: router trpc user
    
    * chore: use new router
    
    * feat: set username

---
+++ lib/api/trpc/routers/user.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+import { publicProcedure, router } from '../server';
+import { userRedis } from '@/lib/api/services/redis';
+
+export const userRouter = router({
+  setUsername: publicProcedure
+    .input(
+      z.object({
+        address: z.string(),
+        username: z.string().min(3).max(20),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      const { address, username } = input;
+      const success = await userRedis.setUsername(address, username);
+      return { success };
+    }),
+
+  getUsername: publicProcedure
+    .input(
+      z.object({
+        address: z.string(),
+      }),
+    )
+    .query(async ({ input }) => {
+      const { address } = input;
+      const username = await userRedis.getUsername(address);
+      return { username };
+    }),
+});
