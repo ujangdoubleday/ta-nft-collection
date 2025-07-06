f9193da96936b716aea4323542598d6604df5e21
ujangbedog <domba88galing@gmail.com>
2025-07-06 18:06:49 +0700

    not use ppr

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,6 @@ const baseConfig = {
   reactStrictMode: true,
   experimental: {
     authInterrupts: true,
-    ppr: 'incremental',
   },
   // Add headers configuration for cookie handling
   async headers() {
