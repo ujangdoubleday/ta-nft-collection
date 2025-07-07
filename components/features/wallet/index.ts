7e7e8b03016fd53bf791d55731273294e90c201c
ujangbedog <domba88galing@gmail.com>
2025-07-08 00:03:50 +0700

    refactor: update wallet components

--- components/features/wallet/index.ts
+++ components/features/wallet/index.ts
@@ -2,4 +2,3 @@ export { useWalletModal } from './components/useWalletModal';
 export type { WalletModalStep } from './components/useWalletModal';
 export { WalletButton } from './components/WalletButton';
 export { WalletModalContent } from './components/WalletModalContent';
-export { ModernWalletModalContent } from './components/ModernWalletModalContent';
