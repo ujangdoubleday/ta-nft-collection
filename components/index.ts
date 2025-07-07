003aeb1222bbf239dafb775e4b27bf5200b49c1c
ujangbedog <domba88galing@gmail.com>
2025-07-07 20:21:04 +0700

    chore: remove export core after deletion

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,5 @@
 ﻿// Core components
-export * from './core/layout';
 export * from './features/layout/public/navigation';
-export * from './core/providers';
 
 // UI components
 export * from './ui/atoms';
