1f122846498a201e991ec28a304d17a7bdff3abb
ujangbedog <domba88galing@gmail.com>
2025-07-07 20:09:18 +0700

    chore: remove deprecated layout and provider components

--- components/core/layout/index.ts
+++
@@ -1,5 +0,0 @@
-﻿// Export components from this directory
-export * from './container';
-export * from './Win98SubMenuBar';
-export * from './Win98Taskbar';
-
--- components/core/navigation/index.ts
+++
@@ -1,5 +0,0 @@
-﻿// Export components from this directory
-export * from './Logo';
-export * from './Navbar';
-export * from './NavLinks';
-
