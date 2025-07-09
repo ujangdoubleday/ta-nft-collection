1f85981ff03b6a2f3dc9e9b5530af4e42b57b429
ujangbedog <domba88galing@gmail.com>
2025-07-09 14:44:52 +0700

    chore: remove export after deletion file

--- components/features/admin/collections/index.ts
+++ components/features/admin/collections/index.ts
@@ -1,8 +1,3 @@
-export * from './UserCollectionsHeader';
-export * from './UserCollectionsList';
-export * from './UserCollectionCard';
-export * from './UserCollectionsContent';
-
 // Collection detail components
 export * from './detail';
 
