06a59c99a972c4b50d6b5697ebc8a1d815c2ac65
ujangbedog <domba88galing@gmail.com>
2025-07-10 11:49:37 +0700

    refactor(user): rename path /my to /user

---
+++ app/(user)/user/collections/page.tsx
@@ -0,0 +1,5 @@
+import { CollectionsContent } from '@/components/features/collections';
+
+export default async function UserCollectionsPage() {
+  return <CollectionsContent role="user" />;
+}
---
+++ app/(user)/user/page.tsx
@@ -0,0 +1,5 @@
+import { OverviewContent } from '@/components/features/user/overview';
+
+export default async function Page() {
+  return <OverviewContent />;
+}
