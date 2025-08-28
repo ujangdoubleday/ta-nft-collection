0f5fb9eb3c71138ae24bb01921cc1e3632edaf08
ilham alfath <97936093+ujangbedog@users.noreply.github.com>
2025-08-28 17:23:05 +0700

    chore: update Hardhat deps & config for Sepolia (#104)
    * chore: update deps for hardhat development
    
    * chore: update hardhat config public node spolia
    
    * chore: convert blob to uin8 -> passing to blob

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/demo",
+      url: process.env.SEPOLIA_RPC_URL || "https://ethereum-sepolia-rpc.publicnode.com",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 11155111,
       gasPrice: "auto",
